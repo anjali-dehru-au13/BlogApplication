2c1474281a7ee7da77385e61e747fadc9b8ddc4b
anjali-dehru-au13 <anjalidehru@gmail.com>
2024-02-26 15:08:11 +0530

    home page posts design

--- client/src/App.js
+++ client/src/App.js
@@ -1,10 +1,12 @@
 import "./App.css";
+import Home from "./pages/Home/Home";
 import Navbar from "./pages/Navbar";
 
 function App() {
  return (
   <div className="App">
    <Navbar />
+   <Home />
   </div>
  );
 }
