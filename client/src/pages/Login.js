3d742f8df307b9a7d5e72eb34a149069b167476c
anjali-dehru-au13 <anjalidehru@gmail.com>
2024-02-27 12:04:28 +0530

    Login register initial page setup

---
+++ client/src/pages/Login.js
@@ -0,0 +1,12 @@
+import React from "react";
+
+const Login = () => {
+ return (
+  <form action="">
+   <input type="text" placeholder="username" />
+   <input type="password" placeholder="Password" />
+  </form>
+ );
+};
+
+export default Login;
