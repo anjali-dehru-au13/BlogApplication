25cf659d4b20959961bc59b85b58890e12a1ac76
anjali-dehru-au13 <anjalidehru@gmail.com>
2024-02-28 19:35:15 +0530

    save data using state

--- client/src/pages/Login.js
+++ client/src/pages/Login.js
@@ -1,6 +1,15 @@
-import React from "react";
+import React, { useState } from "react";
 
 const Login = () => {
+ const [loginInfo, setLoginInfo] = useState();
+ const LoginInfoHandler = (e, field) => {
+  setLoginInfo((prevData) => {
+   return {
+    ...prevData,
+    [field]: e.target.value,
+   };
+  });
+ };
  return (
   <form action="" className="login">
    <h1>Login</h1>
--- client/src/pages/Register.js
+++ client/src/pages/Register.js
@@ -1,13 +1,45 @@
-import React from "react";
+import React, { useState } from "react";
 
 const Register = (props) => {
+ const [userData, setUserData] = useState({});
+ const handleInputData = (e, field) => {
+  setUserData((prevState) => {
+   return {
+    ...prevState,
+    [field]: e.target.value,
+   };
+  });
+ };
+ const register = (e) => {
+  e.preventDefault();
+ };
  return (
-  <form action="" className="register">
+  <form action="" className="register" onSubmit={register}>
    <h1>Register</h1>
-   <input type="email" placeholder="email" />
-   <input type="text" placeholder="username" />
-   <input type="password" placeholder="Password" />
-   <input type="password" placeholder="confirm password" />
+   <input
+    type="email"
+    placeholder="email"
+    value={userData?.email}
+    onChange={(e) => handleInputData(e, "email")}
+   />
+   <input
+    type="text"
+    placeholder="username"
+    value={userData?.username}
+    onChange={(e) => handleInputData(e, "username")}
+   />
+   <input
+    type="password"
+    placeholder="password"
+    value={userData?.password}
+    onChange={(e) => handleInputData(e, "password")}
+   />
+   <input
+    type="password"
+    placeholder="confirm password"
+    value={userData?.confirmPassword}
+    onChange={(e) => handleInputData(e, "confirmPassword")}
+   />
    <button>Register</button>
   </form>
  );
