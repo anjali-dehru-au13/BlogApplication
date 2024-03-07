b647f887bda9a0e9ae50653402f218859e7f9d70
anjali-dehru-au13 <anjalidehru@gmail.com>
2024-03-07 15:32:48 +0530

    controller,model and routes created

--- api/index.js
+++ api/index.js
@@ -5,16 +5,28 @@ const PORT = process.env.PORT || 5000;
 const connectDB = require("./server/config/db");
 
 const cors = require("cors");
-
+const User = require("./server/models/User");
+const bcrypt = require("bcryptjs");
+const salt = bcrypt.genSaltSync(18);
+const router = require("./server/routes/main");
 //connect to db
 connectDB();
 
 app.use(cors());
 app.use(express.json());
-
-app.post("/register", (req, res) => {
+app.use("/api/blogs", router);
+app.post("/register", async (req, res) => {
  const { email, username, password } = req.body;
- res.json({ requestData: { email, username, password } });
+ try {
+  const userData = await User.create({
+   email,
+   username,
+   password: bcrypt.hashSync(password, salt),
+  });
+  res.json(userData);
+ } catch (error) {
+  res.status(400).json(error);
+ }
 });
 
 app.listen(PORT, () => {
---
+++ api/server/models/User.js
@@ -0,0 +1,22 @@
+const mongoose = require("mongoose");
+
+const { Schema, model } = mongoose;
+
+const UserSchema = new Schema({
+ email: {
+  type: String,
+  required: true,
+ },
+ username: {
+  type: String,
+  required: true,
+  min: 4,
+  unique: true,
+ },
+ password: {
+  type: String,
+  required: true,
+ },
+});
+const UserModel = model("User", UserSchema);
+module.exports = UserModel;
