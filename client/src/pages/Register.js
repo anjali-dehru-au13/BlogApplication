99b7eeb8e6fbb6f4d30b1c47c7934ea7f3b5a663
anjali-dehru-au13 <anjalidehru@gmail.com>
2024-03-09 13:17:04 +0530

    regsiter Page validations added

--- client/src/pages/Register.js
+++ client/src/pages/Register.js
@@ -10,38 +10,69 @@ const Register = (props) => {
    };
   });
  };
- const register = (e) => {
+
+ //  const getRegister = () => {
+ //   fetch("http://localhost:5000/register", {
+ //    method: "POST",
+ //    body: JSON.stringify(userData),
+ //    headers: { "content-Type": "application/json" },
+ //   })
+ //    .then((response) => {
+ //     return console.log(response);
+ //    })
+ //    .catch((error) => {
+ //     return console.log(error);
+ //    });
+ //  };
+ const register = async (e) => {
   e.preventDefault();
+  const response = await fetch("http://localhost:5000/register", {
+   method: "POST",
+   body: JSON.stringify(userData),
+   headers: { "content-Type": "application/json" },
+  });
+  if (response.status === 200) {
+   alert("registration successful");
+  } else {
+   alert("registration failed");
+  }
+  // getRegister();
  };
  return (
-  <form action="" className="register" onSubmit={register}>
-   <h1>Register</h1>
-   <input
-    type="email"
-    placeholder="email"
-    value={userData?.email}
-    onChange={(e) => handleInputData(e, "email")}
-   />
-   <input
-    type="text"
-    placeholder="username"
-    value={userData?.username}
-    onChange={(e) => handleInputData(e, "username")}
-   />
-   <input
-    type="password"
-    placeholder="password"
-    value={userData?.password}
-    onChange={(e) => handleInputData(e, "password")}
-   />
-   <input
-    type="password"
-    placeholder="confirm password"
-    value={userData?.confirmPassword}
-    onChange={(e) => handleInputData(e, "confirmPassword")}
-   />
-   <button>Register</button>
-  </form>
+  <div className="formWrapperDiv">
+   <form action="" className="register" onSubmit={register}>
+    <h1>Register</h1>
+    <input
+     type="email"
+     placeholder="email"
+     value={userData?.email}
+     onChange={(e) => handleInputData(e, "email")}
+     required
+    />
+    <input
+     type="text"
+     placeholder="username"
+     value={userData?.username}
+     onChange={(e) => handleInputData(e, "username")}
+     required
+    />
+    <input
+     type="password"
+     placeholder="password"
+     value={userData?.password}
+     onChange={(e) => handleInputData(e, "password")}
+     required
+    />
+    <input
+     type="password"
+     placeholder="confirm password"
+     value={userData?.confirmPassword}
+     onChange={(e) => handleInputData(e, "confirmPassword")}
+     required
+    />
+    <button>Register</button>
+   </form>
+  </div>
  );
 };
 
