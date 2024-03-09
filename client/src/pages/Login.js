e3d6a0f2e5ec7f370ddd6d237ac2235b1ca26d45
anjali-dehru-au13 <anjalidehru@gmail.com>
2024-03-09 13:39:29 +0530

    Login Page styling

--- client/src/pages/Login.js
+++ client/src/pages/Login.js
@@ -11,12 +11,14 @@ const Login = () => {
   });
  };
  return (
-  <form action="" className="login">
-   <h1>Login</h1>
-   <input type="text" placeholder="username" />
-   <input type="password" placeholder="Password" />
-   <button>Login</button>
-  </form>
+  <div className="formWrapperDiv">
+   <form action="" className="login">
+    <h1>Login</h1>
+    <input type="text" placeholder="username" />
+    <input type="password" placeholder="Password" />
+    <button>Login</button>
+   </form>
+  </div>
  );
 };
 
