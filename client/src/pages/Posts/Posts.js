d8200c1fe839409703f66c269e1efc62065fcb7d
anjali-dehru-au13 <anjalidehru@gmail.com>
2024-03-09 19:48:06 +0530

    posts page api integration

--- client/src/pages/Posts/Posts.js
+++ client/src/pages/Posts/Posts.js
@@ -1,39 +1,54 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import ramenBowl from "../../assets/ramen-bowl.png";
 import "./Posts.css";
-const Home = () => {
+import axios from "axios";
+const Posts = () => {
+ const [posts, setPosts] = useState({});
+
+ const [pending, setPending] = useState(true);
+ async function fetchListOfBlogs() {
+  const response = await axios.get("http://localhost:5000/api/blogs");
+  const result = await response.data;
+  if (result && result?.blogList && result?.blogList?.length) {
+   setPosts(result?.blogList);
+   setPending(false);
+  } else {
+   setPending(true);
+  }
+ }
+ useEffect(() => {
+  fetchListOfBlogs();
+ }, []);
+
  return (
-  <div className="post">
-   <img src={ramenBowl} alt="post image" width="50%" />
-   <div
-    style={
-     {
-      // border: "1px solid #000",
-      // borderRadius: "18px",
-      // padding: "4%",
-     }
-    }
-    className="postDetails"
-   >
-    <h2 className="title"> Where can I get some?</h2>
-    <p style={{}}>
-     <a>Author</a>
-     <time>2024-01-01 16:45</time>
-    </p>
-    <p>
-     Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod
-     tempor incididunt ut labore et dolore magna aliqua. Habitant morbi
-     tristique senectus et netus et malesuada fames. Lectus magna fringilla urna
-     porttitor rhoncus dolor purus non. Id venenatis a condimentum vitae sapien
-     pellentesque. Accumsan tortor posuere ac ut consequat semper viverra. Dui
-     accumsan sit amet nulla. Sapien eget mi proin sed libero enim sed. Cras
-     tincidunt lobortis feugiat vivamus at augue eget arcu dictum. Quam
-     elementum pulvinar etiam non quam lacus. Cursus eget nunc scelerisque
-     viverra. Tincidunt vitae semper quis lectus nulla at volutpat diam ut.
-    </p>
+  <>
+   <div className="heading">
+    <h2>Recent Blogs</h2>
    </div>
-  </div>
+   <div className="BlogCards">
+    {pending ? (
+     <h1 style={{ padding: "8%" }}>Loading...</h1>
+    ) : (
+     posts.map((post) => {
+      return (
+       <div className="blogList" key={post?.id}>
+        <div className="imageDiv">
+         <img src={ramenBowl} alt="post image" className="blogImage" />
+        </div>
+        <div className="blogDetails">
+         <h2 className="title"> {post?.title}</h2>
+         <p style={{}}>
+          <time>{post?.createdAt}</time>
+         </p>
+         <p>{post?.description}</p>
+        </div>
+       </div>
+      );
+     })
+    )}
+   </div>
+  </>
  );
 };
 
-export default Home;
+export default Posts;
