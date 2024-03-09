5c40842da479ab77d7e399d3cebf807e02cdcbc1
anjali-dehru-au13 <anjalidehru@gmail.com>
2024-03-09 12:51:25 +0530

    home page styling text added

--- client/src/pages/Home/Home.js
+++ client/src/pages/Home/Home.js
@@ -1,11 +1,19 @@
 import React from "react";
-import bgImage from "../../assets/bg.jpg";
+import Posts from "../Posts/Posts";
+import "./home.css";
 const Home = () => {
  return (
   <div>
-   <img src={bgImage} alt="background-image" width={"100%"} />
+   <div className="FirstDiv">
+    {/* <img src={bgImage} alt="background-image" width={"100%"} /> */}
+    <p className="titleText">Explore New Horizons</p>
+    <p className="titleText subText"> And Dive Deeper into creativity</p>
+   </div>
+   <Posts />
   </div>
  );
 };
 
 export default Home;
+
+const styles = {};
