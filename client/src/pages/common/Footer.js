25d2d5b6fb98450690e5e7402e0dd2089bedbd5e
anjali-dehru-au13 <anjalidehru@gmail.com>
2024-03-19 11:16:05 +0530

    Footer Changes

--- client/src/pages/common/Footer.js
+++ client/src/pages/common/Footer.js
@@ -1,7 +1,31 @@
 import React from "react";
+import { Link } from "react-router-dom";
 
 const Footer = () => {
- return <div className="footerDiv">Footer</div>;
+ return (
+  <div className="footerDiv flex flex-row backdrop:backdrop-opacity-25 ">
+   <div className="basis-1/2">
+    <h3>Logo Here</h3>
+   </div>
+   <div className="flex flex-row basis-1/2 justify-normal align-middle">
+    {[
+     ["Home", "/"],
+     ["About", "/about"],
+     ["Blogs", "/posts"],
+     ["Login", "/login"],
+     ["Register", "/signUp"],
+     ["Contact", "/contact"],
+    ].map(([title, url]) => (
+     <Link
+      to={url}
+      className="flex justify-center basis-1/6 text-m  hover:border-y-1 hover:border-x-2 "
+     >
+      <h3>{title}</h3>
+     </Link>
+    ))}
+   </div>
+  </div>
+ );
 };
 
 export default Footer;
